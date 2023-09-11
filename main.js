128fbd47a327fd297972487f20291441fea59d62
Ayushman Rathore <ayushsg0412@gmail.com>
2023-09-11 23:38:16 +0530

    add a basic crawler

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const { normalizeURL, getURLsFromHTML, crawlPage } = require("./crawl.js");
 
-function main() {
-  if (process.argv.length < 3) {
-    console.log("no website provided");
-  }
-  if (process.argv.length > 3) {
-    console.log("too many arguments provided");
-  }
+async function main() {
+    if (process.argv.length < 3) {
+        console.log("no website provided");
+    }
+    if (process.argv.length > 3) {
+        console.log("too many arguments provided");
+    }
 
-  const baseURL = process.argv[2];
+    const baseURL = process.argv[2];
 
-  console.log(`starting crawl of: ${baseURL}...`);
-  crawlPage(baseURL);
+    console.log(`starting crawl of: ${baseURL}...`);
+    const pages = await crawlPage(baseURL, baseURL, {});
+    console.log(pages);
 }
 
 main();
