9b7fcd03586cd6437d126f962a5e5dc352bdaff1
Ayushman Rathore <ayushsg0412@gmail.com>
2023-09-11 22:59:44 +0530

    add basic crawlPage to see output and write some more tests

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const { normalizeURL, getURLsFromHTML, crawlPage } = require("./crawl.js");
+
+function main() {
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+  }
+  if (process.argv.length > 3) {
+    console.log("too many arguments provided");
+  }
+
+  const baseURL = process.argv[2];
+
+  console.log(`starting crawl of: ${baseURL}...`);
+  crawlPage(baseURL);
+}
+
+main();
